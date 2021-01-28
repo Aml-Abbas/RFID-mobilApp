f2fbe542b7638e1874aec27cc168c8a7a7205da6
Aml-Abbas <aml.abbas@hotmail.com>
2021-01-28 19:24:34 +0100

    Deleted som comments

--- app/src/main/java/com/example/rfid_mobilapp/MainActivity.java
+++ app/src/main/java/com/example/rfid_mobilapp/MainActivity.java
@@ -19,7 +19,6 @@ import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 
 
-
 public class MainActivity extends AppCompatActivity {
     TextView tagContentTextView;
     NfcAdapter mNfcAdapter;
@@ -61,15 +60,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
         tagContentTextView.setText("");
-        String payload= getPayload(intent);
+        String payload = getPayload(intent);
         tagContentTextView.setText(payload);
-       //  https://aml-abbas.github.io/RFID-Pages/
-         intent= new Intent(Intent.ACTION_VIEW);
-         intent.setData(Uri.parse("https://aml-abbas.github.io/RFID-Pages/"));
+
+        intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("https://aml-abbas.github.io/RFID-Pages/"));
         Intent chooser = Intent.createChooser(intent, "Open webbsite for item:" + payload);
-         if (intent.resolveActivity(getPackageManager())!= null){
-             startActivity(chooser);
-         }
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(chooser);
+        }
     }
 
     private String getPayload(Intent intent) {
@@ -89,7 +88,7 @@ public class MainActivity extends AppCompatActivity {
 
                 int offset = 0;  // offset of first block to read
                 int blocks = 0;  // number of blocks to read
-                byte[] cmd = new byte[] {
+                byte[] cmd = new byte[]{
                         (byte) 0x60,  // flags: addressed (= UID field present)
                         (byte) 0x23, // command: READ MULTIPLE BLOCKS
                         (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,  // placeholder for tag UID
@@ -99,21 +98,12 @@ public class MainActivity extends AppCompatActivity {
                 System.arraycopy(tagId, 0, cmd, 2, 8);
 
                 byte[] response = nfcV.transceive(cmd);
-               // byte[] response2 = nfcV.transceive(new byte[] {(byte)0x00,(byte)0x32,(byte)0x00,(byte)0x01});
 
                 byte[] primeItemId = new byte[16];
                 byte[] primeItemId2 = new byte[16];
-                System.out.println();
-                System.out.println();
-                System.out.println();
-                System.out.println();
 
                 System.out.println("the respons size is: " + response.length);
-              //  System.out.println("the respons2 size is: " + response2.length);
-                System.out.println();
-                System.out.println();
-                System.out.println();
-                System.out.println();
+
 
                 for (int i = 0; i < 6; i++) {
                     primeItemId[i] = response[i + 3];
@@ -123,9 +113,9 @@ public class MainActivity extends AppCompatActivity {
                 if (primeItemId[0] == 1) {
                     inAlternativeItemId = true;
 
-                     offset = 32;  // offset of first block to read
-                     blocks = 0;  // number of blocks to read
-                    cmd = new byte[] {
+                    offset = 32;  // offset of first block to read
+                    blocks = 0;  // number of blocks to read
+                    cmd = new byte[]{
                             (byte) 0x60,  // flags: addressed (= UID field present)
                             (byte) 0x23, // command: READ MULTIPLE BLOCKS
                             (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,  // placeholder for tag UID
@@ -139,7 +129,7 @@ public class MainActivity extends AppCompatActivity {
                     for (int k = 0; k < 16; k++) {
                         primeItemId2[k] = OptionalBlock[k + 5];
                     }
-                    noId= false;
+                    noId = false;
                 } else {
                     for (int i = 0; i < primeItemId.length; i++) {
                         if (primeItemId[i] != 0) {
@@ -148,39 +138,39 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
 
-                if (noId){
-                    payloadString= new String("NO ID");
-                }else if (primeItemId2.length ==0){
+                if (noId) {
+                    payloadString = new String("NO ID");
+                } else if (primeItemId2.length == 0) {
 
                     ByteBuffer buffer = ByteBuffer.wrap(primeItemId);
                     buffer.order(ByteOrder.LITTLE_ENDIAN);  // if you want little-endian
                     int result = buffer.getShort();
-                    payloadString= String.valueOf(result);
+                    payloadString = String.valueOf(result);
 
-                }else {
-                    byte[] newPrimeItemId= new byte[16*2];
-                    for (int i=0;i<16; i++){
-                        newPrimeItemId[i]= primeItemId[i];
+                } else {
+                    byte[] newPrimeItemId = new byte[16 * 2];
+                    for (int i = 0; i < 16; i++) {
+                        newPrimeItemId[i] = primeItemId[i];
                     }
-                    for (int i=0;i<16; i++){
-                        newPrimeItemId[i+16]= primeItemId[i];
+                    for (int i = 0; i < 16; i++) {
+                        newPrimeItemId[i + 16] = primeItemId[i];
                     }
 
                     ByteBuffer buffer = ByteBuffer.wrap(newPrimeItemId);
                     buffer.order(ByteOrder.LITTLE_ENDIAN);  // if you want little-endian
                     int result = buffer.getShort();
-                    payloadString= String.valueOf(result);
+                    payloadString = String.valueOf(result);
                 }
 
 
                 nfcV.close();
 
-            }catch (IOException ioException) {
+            } catch (IOException ioException) {
                 Toast.makeText(this, "Failed to read tag", Toast.LENGTH_LONG).show();
 
             }
         }
-            return payloadString;
+        return payloadString;
 
     }
 
