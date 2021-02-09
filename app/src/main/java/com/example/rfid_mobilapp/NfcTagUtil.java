dbed26893cfe943bda69dbbf2a5a7c135422292f
aishauk <aishaugljanin@gmail.com>
2021-02-09 16:05:39 +0100

    Changed method getResults where we return a String in UTF_8.

--- app/src/main/java/com/example/rfid_mobilapp/NfcTagUtil.java
+++ app/src/main/java/com/example/rfid_mobilapp/NfcTagUtil.java
@@ -13,6 +13,7 @@ import android.widget.Toast;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
 public class NfcTagUtil {
@@ -164,12 +165,7 @@ public class NfcTagUtil {
     }
 
     private static String getResult(byte[] primeItemId) {
-        String text = "";
-        if (null == primeItemId) return text;
-        for (byte element : primeItemId) {
-            text += String.valueOf((char) element);
-        }
-        return text;
+        return new String(primeItemId, StandardCharsets.UTF_8);
     }
 
     private static boolean isEmtpy(byte[] primeItemId) {
