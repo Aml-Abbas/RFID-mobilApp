6683d8b4fd68fb3d11b0665f70cea30248c4c6ef
aishauk <aishaugljanin@gmail.com>
2021-02-09 16:26:38 +0100

    Removed logD to check itemID

--- app/src/main/java/com/example/rfid_mobilapp/NfcTagUtil.java
+++ app/src/main/java/com/example/rfid_mobilapp/NfcTagUtil.java
@@ -90,9 +90,6 @@ public class NfcTagUtil {
                 nfcV.connect();
                 byte[] tagId = tag.getId();
                 byte[] response = nfcV.transceive(getCommandWriteSingleBlock(tagId, itemId));
-
-                Log.d("I am after respons", "Item id = " + itemId);
-
                 nfcV.close();
                 Toast.makeText(activity, "Success to write to the tag. The new itemId is "+itemId , Toast.LENGTH_LONG).show();
             } catch (IOException ioException) {
