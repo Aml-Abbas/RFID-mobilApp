6cc045065e21552d9cf3bf386407093359c2da5a
Aml-Abbas <aml.abbas@hotmail.com>
2021-02-26 14:53:55 +0100

    added setLovale method to change the language and getLanguageStuff to get the language from the list

--- app/src/main/java/com/example/rfid_mobilapp/MainActivity.java
+++ app/src/main/java/com/example/rfid_mobilapp/MainActivity.java
@@ -3,10 +3,22 @@ package com.example.rfid_mobilapp;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.net.Uri;
 import android.nfc.NfcAdapter;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -17,6 +29,9 @@ public class MainActivity extends AppCompatActivity {
     String checkValue;
     tagInsertDialog dialog;
 
+    Spinner spinner;
+    Locale myLocale;
+    String currentLanguage = "en", currentLang;
 
     private static final boolean checkIn = true;
     private static final boolean checkOut = false;
@@ -30,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
         mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
         mainActivityContext = this;
 
+        getLanguageStruff();
         Intent intent = getIntent();
         newItemId="";
         if (Intent.ACTION_VIEW.equals(intent.getAction())) {
@@ -41,6 +57,41 @@ public class MainActivity extends AppCompatActivity {
         }
         openDialog();
     }
+
+    private void getLanguageStruff() {
+        currentLanguage = getIntent().getStringExtra(currentLang);
+
+        spinner = (Spinner) findViewById(R.id.spinner);
+
+        List<String> list = new ArrayList<String>();
+
+        list.add("Select language");
+        list.add("English");
+        list.add("Svenska");
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, list);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
+                switch (position) {
+                    case 0:
+                        break;
+                    case 1:
+                        setLocale("en");
+                        break;
+                    case 2:
+                        setLocale("sv");
+
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
+    }
+
     @Override
     protected void onResume() {
         super.onResume();
@@ -85,5 +136,29 @@ public class MainActivity extends AppCompatActivity {
     private void openDialog(){
         dialog = new tagInsertDialog();
         dialog.show(getSupportFragmentManager(), "dialog");
+    }
+    public void setLocale(String localeName) {
+        if (!localeName.equals(currentLanguage)) {
+            myLocale = new Locale(localeName);
+            Resources res = getResources();
+            DisplayMetrics dm = res.getDisplayMetrics();
+            Configuration conf = res.getConfiguration();
+            conf.locale = myLocale;
+            res.updateConfiguration(conf, dm);
+            Intent refresh = new Intent(this, MainActivity.class);
+            refresh.putExtra(currentLang, localeName);
+            startActivity(refresh);
+        } else {
+            Toast.makeText(MainActivity.this, "", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void onBackPressed() {
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+        finish();
+        System.exit(0);
     }
 }
