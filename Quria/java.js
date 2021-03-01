ae2adb9cf87996e2a1064d3dc7b177270c1ac4ec
Aml Abbas <aml.abbas@hotmail.com>
2021-03-01 14:47:20 +0100

    Update java.js

--- Quria/java.js
+++ Quria/java.js
@@ -13,26 +13,8 @@ function getIdFromParam() {
     var itemIdP = document.getElementById("item-id");
     var itemId = getParameterByName('itemId');
     itemIdP.textContent = `item id: ${getParameterByName('itemId')}`;
- //   createLink(itemId);
 }; 
 function openAppen(){
     var itemId = document.getElementById("item-id-input").value;
-
     location.href = "androidrfid://primaryid?itemid=" + itemId;
-
 }
-
-
-
-/*  function createLink(itemId) {
-    androiduri = "androidrfid://primaryid?itemid=" + itemId;
-    var aTag = document.getElementById("link");
-    aTag.setAttribute("href", androiduri);
-} 
-
-function writeToTag() {
-    var itemId = document.getElementById("item-id-input").value;
-    var itemIdP = document.getElementById("item-id");
-    itemIdP.textContent = `item id: ` + itemId;
-    createLink(itemId);
-} */
