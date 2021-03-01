accc6add22c885984116e053953bd85a15c4076b
Aml-Abbas <aml.abbas@hotmail.com>
2021-03-01 14:57:43 +0100

    Create a new class to test the NfcTagUtil class

---
+++ app/src/test/java/com/example/rfid_mobilapp/NfcTagUtilTest.java
@@ -0,0 +1,13 @@
+package com.example.rfid_mobilapp;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class NfcTagUtilTest {
+
+    @Test
+    public void setBarcodeTest() {
+
+    }
+}
