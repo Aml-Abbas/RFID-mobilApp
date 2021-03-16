6fc244520662c96c8f9b2173c46b4c47e2fd7aad
Aml-Abbas <aml.abbas@hotmail.com>
2021-03-16 19:35:08 +0100

    Added the class TagProgressDialog to show up a dialog with a progress indicator

---
+++ app/src/main/java/com/example/rfid_mobilapp/TagProgressDialog.java
@@ -0,0 +1,30 @@
+package com.example.rfid_mobilapp;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+public class TagProgressDialog extends AppCompatDialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity()).setView(R.layout.dialoy_layout);
+        builder.setTitle(R.string.welcome)
+                .setMessage(R.string.place_tag)
+
+                .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        closeApp(getContext());
+                    }
+                });
+        return builder.create();
+    }
+
+    private void closeApp( Context context) {
+        ((Activity)context).finish();
+    }
+}
