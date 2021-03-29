6c8290179a047dadf5410702380be334b8eb0c76
Aml-Abbas <aml.abbas@hotmail.com>
2021-03-29 14:12:09 +0200

    Notification to show that the app is running The notification cannot be dismissed A button in the notification to open the app

--- app/src/main/java/com/example/rfid_mobilapp/MainActivity.java
+++ app/src/main/java/com/example/rfid_mobilapp/MainActivity.java
@@ -1,45 +1,24 @@
 package com.example.rfid_mobilapp;
 
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.content.res.Configuration;
-import android.content.res.Resources;
-import android.net.Uri;
 import android.nfc.NfcAdapter;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
-import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.UnsupportedEncodingException;
-import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
-import java.util.Base64;
-import java.util.List;
 import java.util.Locale;
-import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 
 public class MainActivity extends AppCompatActivity {
     private final String TAG = MainActivity.class.getSimpleName();
@@ -64,47 +43,6 @@ public class MainActivity extends AppCompatActivity {
 
         Intent serviceIntent = new Intent(this, SocketServerService.class);
         startService(serviceIntent);
-
-        //try {
-        //ServerSocket server = new ServerSocket(8080);
-        //Log.d(TAG, "Server has started on localhost.\r\nWaiting for a connection...");
-
-/*
-        Thread thread = new Thread(() -> {
-            String host = "localhost";
-            int port = 8888;
-            try {
-                InetSocketAddress listenAddress = new InetSocketAddress(host, port);
-                SocketServer server = new SocketServer(listenAddress);
-                server.run();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
-        thread.start();
-*/
-
-          /* InputStream in = client.getInputStream();
-            OutputStream out = client.getOutputStream();
-            Scanner s = new Scanner(in, "UTF-8");
-            String data = s.useDelimiter("\\r\\n\\r\\n").next();
-            Matcher get = Pattern.compile("^GET").matcher(data);
-            if (get.find()) {
-                Matcher match = Pattern.compile("Sec-WebSocket-Key: (.*)").matcher(data);
-                match.find();
-                byte[] response = ("HTTP/1.1 101 Switching Protocols\r\n"
-                        + "Connection: Upgrade\r\n"
-                        + "Upgrade: websocket\r\n"
-                        + "Sec-WebSocket-Accept: "
-                        + Base64.getEncoder().encodeToString(MessageDigest.getInstance("SHA-1").digest((match.group(1) + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11").getBytes("UTF-8")))
-                        + "\r\n\r\n").getBytes("UTF-8");
-                out.write(response, 0, response.length);
-        doCheckIn = uri.getQueryParameter("doCheckIn");
-    }
-    chooseLanguage();*/
-        //} catch (IOException ioException) {
-        //    ioException.printStackTrace();
-        //}
     }
 
 
--- app/src/main/java/com/example/rfid_mobilapp/SocketServerService.java
+++ app/src/main/java/com/example/rfid_mobilapp/SocketServerService.java
@@ -1,15 +1,18 @@
 package com.example.rfid_mobilapp;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
 
 import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
 
 import java.net.InetSocketAddress;
 
 public class SocketServerService extends Service {
-
     @Override
     public void onCreate() {
         super.onCreate();
@@ -17,6 +20,7 @@ public class SocketServerService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        createNotification();
         Thread thread = new Thread(() -> {
             String host = "localhost";
             int port = 8888;
@@ -41,6 +45,25 @@ public class SocketServerService extends Service {
     @Override
     public IBinder onBind(Intent intent) {
         return null;
+    }
+
+    private void createNotification() {
+        Intent openAppIntent = new Intent(this, MainActivity.class);
+        openAppIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+        PendingIntent openAppPendingIntent =
+                PendingIntent.getActivity(this, 0, openAppIntent, 0);
+
+        NotificationCompat.Builder SocketServerServiceNotification = new NotificationCompat.Builder(this, "q")
+                .setSmallIcon(R.drawable.message_24)
+                .setContentTitle("SocketServerService")
+                .setContentText("The service is running")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .addAction(R.drawable.back_24, "Back to the app", openAppPendingIntent)
+                .setOngoing(true);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(0, SocketServerServiceNotification.build());
     }
 
 }
