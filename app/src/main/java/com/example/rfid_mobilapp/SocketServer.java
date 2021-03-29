cd3ab780f6e83db20ed92140279e8893ae982fa6
Aml-Abbas <aml.abbas@hotmail.com>
2021-03-29 09:59:56 +0200

    changed from printOut to log.d to easy find the prints out

--- app/src/main/java/com/example/rfid_mobilapp/SocketServer.java
+++ app/src/main/java/com/example/rfid_mobilapp/SocketServer.java
@@ -11,26 +11,27 @@ import org.java_websocket.server.WebSocketServer;
 
 public class SocketServer extends WebSocketServer {
 
-    public SocketServer(InetSocketAddress address) {
+    private final String TAG = SocketServer.class.getSimpleName();
 
+    public SocketServer(InetSocketAddress address) {
         super(address);
     }
 
     @Override
     public void onOpen(WebSocket conn, ClientHandshake handshake) {
         conn.send("Welcome to the server!"); //This method sends a message to the new client
-        broadcast( "new connection: " + handshake.getResourceDescriptor() ); //This method sends a message to all clients connected
-        System.out.println("new connection to " + conn.getRemoteSocketAddress());
+        broadcast("new connection: " + handshake.getResourceDescriptor()); //This method sends a message to all clients connected
+        Log.d(TAG, "new connection to " + conn.getRemoteSocketAddress());
     }
 
     @Override
     public void onClose(WebSocket conn, int code, String reason, boolean remote) {
-        System.out.println("closed " + conn.getRemoteSocketAddress() + " with exit code " + code + " additional info: " + reason);
+        Log.d(TAG, "closed " + conn.getRemoteSocketAddress() + " with exit code " + code + " additional info: " + reason);
     }
 
     @Override
     public void onMessage(WebSocket conn, String message) {
-        System.out.println("received message from "	+ conn.getRemoteSocketAddress() + ": " + message);
+        Log.d(TAG, "received message from " + conn.getRemoteSocketAddress() + ": " + message);
         if (message.equals("ping")) {
             // "command" ping received, sending echo.
             conn.send("echo");
@@ -38,18 +39,18 @@ public class SocketServer extends WebSocketServer {
     }
 
     @Override
-    public void onMessage( WebSocket conn, ByteBuffer message ) {
-        System.out.println("received ByteBuffer from "	+ conn.getRemoteSocketAddress());
+    public void onMessage(WebSocket conn, ByteBuffer message) {
+        Log.d(TAG, "received ByteBuffer from " + conn.getRemoteSocketAddress());
     }
 
     @Override
     public void onError(WebSocket conn, Exception ex) {
-        System.err.println("an error occurred on connection " + conn.getRemoteSocketAddress()  + ":" + ex);
+        Log.d(TAG, "an error occurred on connection " + conn.getRemoteSocketAddress() + ":" + ex);
     }
 
     @Override
     public void onStart() {
-        System.out.println("server started successfully");
+        Log.d(TAG, "server started successfully");
     }
 
 
