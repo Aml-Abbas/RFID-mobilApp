188159f417396b7da863c2bd591c7aa6e52f188f
Aml-Abbas <aml.abbas@hotmail.com>
2021-04-09 17:12:06 +0200

    create NfcActivity for Nfc intents

---
+++ app/src/main/java/com/example/rfid_mobilapp/NfcActivity.java
@@ -0,0 +1,14 @@
+package com.example.rfid_mobilapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NfcActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nfc);
+    }
+}
