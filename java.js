c45d40d622caab3dc264186dc151f5028b8899a5
aishauk <aishauk@users.noreply.github.com>
2021-04-09 12:07:35 +0000

    deploy: 6e037bc131cb00626f2863269a3afb6ba6247462

---
+++ java.js
@@ -0,0 +1,52 @@
+/* var androiduri;
+
+function getParameterByName(name, url = window.location.href) {
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+function getIdFromParam() {
+    var itemIdP = document.getElementById("item-id");
+    var itemId = getParameterByName('itemId');
+    itemIdP.textContent = `item id: ${getParameterByName('itemId')}`;
+}; 
+function openAppen(){
+    var itemId = document.getElementById("item-id-input").value;
+    location.href = "androidrfid://primaryid?itemid=" + itemId;
+} */
+
+function write_item_id() {
+  var itemId = document.getElementById("item-id-input").value;
+  ws.send('{"toDo": "write", "value": "'+itemId+'"}');
+}
+
+function do_check_in(value) {
+  ws.send('{"toDo": "doCheckIn", "value": "'+value+'"}');
+}
+
+
+var ip = "localhost";
+var port = "8888";
+var ws = new WebSocket("ws://" + ip + ":" + port);
+ws.onopen = function() {
+  var doSendPing = confirm('connected! Send ping? Otherwise we will send "bla bla".');
+  if (doSendPing) {
+    ws.send('ping');
+  } else {
+    ws.send('bla bla');
+  }
+};
+ws.onmessage = function (event) {
+  if (event.data === 'echo') {
+    alert(event.data);
+  }
+}
+
+ws.onclose = function (even) {
+  alert("Connection Closed");
+  ws.close();
+}
