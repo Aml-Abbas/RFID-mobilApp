c4ff4962b07e1ba54031e74d94500a737cda1123
Aml-Abbas <aml.abbas@hotmail.com>
2021-04-10 10:17:40 +0200

    - created a new activity for the nfc intents only and moved the methods from the MainActivity to the new activity - added a text view to te new activity to show the string with place_tag for now, cause it must start one time before it can write or check in without opening -> will be fixed

--- app/src/main/java/com/example/rfid_mobilapp/NfcActivity.java
+++ app/src/main/java/com/example/rfid_mobilapp/NfcActivity.java
@@ -2,13 +2,98 @@ package com.example.rfid_mobilapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 public class NfcActivity extends AppCompatActivity {
+    private static final String TAG = NfcTagUtil.class.getSimpleName();
+    static String newItemId;
+    static String doCheckIn;
+    private static final boolean checkIn = true;
+    private static final boolean checkOut = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nfc);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        Log.d(TAG, "new intent");
+        if (doCheckIn != null) {
+            Log.d(TAG, "will do check");
+            if (doCheckIn.equals("false")) {
+                NfcTagUtil.check(intent, this, checkOut);
+                Log.d(TAG, "out");
+            } else {
+                NfcTagUtil.check(intent, this, checkIn);
+                Log.d(TAG, "in");
+            }
+            doCheckIn = null;
+            newItemId = "";
+        } else if (newItemId != null && !newItemId.isEmpty()) {
+            NfcTagUtil.writeNewItemId(newItemId, intent, this);
+            newItemId = "";
+        } /*else {
+            String payload = NfcTagUtil.getItemId(intent, this);
+            serviceIntent = new Intent(this, SocketServerService.class);
+            serviceIntent.setAction("READ_TAG");
+            serviceIntent.putExtra("itemId", payload);
+            startService(serviceIntent);
+        }*/
+        moveTaskToBack(true);
+    }
+
+
+    public static void setItemId(String itemId) {
+        Log.d(TAG, "1. item id is now" + itemId);
+        newItemId = itemId;
+    }
+
+    public static void setDoCheckIn(String value) {
+        Log.d(TAG, "value now is " + value);
+        doCheckIn = value;
+    }
+
+
+
+
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "on Stop");
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "on onDestroy");
+    }
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "on onReStart");
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+      //  NfcTagUtil.enableNFCInForeground(mNfcAdapter, this, getClass());
+        Log.d(TAG, "on Resume");
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "on onStart");
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+       // NfcTagUtil.disableNFCInForeground(mNfcAdapter, this);
+        Log.d(TAG, "on pause");
     }
 }
