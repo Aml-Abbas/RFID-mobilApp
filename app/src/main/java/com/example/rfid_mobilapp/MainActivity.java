07a07b6db82a98e9d49e72451c200f8f237e5724
Aml-Abbas <aml.abbas@hotmail.com>
2021-04-12 11:26:43 +0200

    changed the name of the method

--- app/src/main/java/com/example/rfid_mobilapp/MainActivity.java
+++ app/src/main/java/com/example/rfid_mobilapp/MainActivity.java
@@ -41,12 +41,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getIds();
         setUpSpinner(spinner);
-        setUSocketServiceButton();
+        setUpSocketServiceButton();
         serviceIntent = new Intent(this, SocketServerService.class);
         startService(serviceIntent);
     }
 
-    private void setUSocketServiceButton() {
+    private void setUpSocketServiceButton() {
         socketServiceSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
