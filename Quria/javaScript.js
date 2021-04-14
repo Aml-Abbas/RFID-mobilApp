ab0b30d6e2cea22141064597de5289bc79a9b51d
Aml-Abbas <aml.abbas@hotmail.com>
2021-04-14 13:07:56 +0200

    create a modal to show the fail or success msg instead of a alert

--- Quria/javaScript.js
+++ Quria/javaScript.js
@@ -1,7 +1,16 @@
 var place_tag_modal = document.getElementById("place-tag-modal");
 var write_item_id_modal = document.getElementById("write-item-id-modal");
+var success_modal = document.getElementById("success-modal");
+var failed_modal = document.getElementById("failed-modal");
+
 var place_tag_close = document.getElementById("place-tag-close");
 var write_item_id_close = document.getElementById("write-item-id-close");
+var success_close = document.getElementById("success-close");
+var failed_close = document.getElementById("failed-close");
+
+var success_text = document.getElementById("success-text"); 
+var failed_text = document.getElementById("failed-text"); 
+
 var itemIdP = document.getElementById("book_id"); 
 var book_image = document.getElementById("book_pic");
 var book_name = document.getElementById("book_name");
@@ -39,7 +48,6 @@ var books= [
     var bookItemId = itemId.toString().trim();
 
     if(bookItemId.localeCompare(book_id)== 0){
-
       itemIdP.innerHTML  = 'item id: '+book_id;
       book_name.innerHTML=  'name of the book is: '+books[i].name;
       book_image.src = 'pic/books/'+books[i].picture;
@@ -48,7 +56,7 @@ var books= [
   }
 }
 
-function showModal(status){
+function showPlaceTagModal(status){
   if(status.localeCompare('false')==0){
     place_tag_modal.style.display = "none";
   }else{
@@ -56,17 +64,34 @@ function showModal(status){
   }
 }
 place_tag_close.onclick = function() {
-showModal('false');
+  showPlaceTagModal('false');
 }
 
 write_item_id_close.onclick = function() {
   write_item_id_modal.style.display = "none";
 }
 
+success_close.onclick = function() {
+  success_modal.style.display = "none";
+}
+failed_close.onclick = function() {
+  failed_modal.style.display = "none";
+}
+
+function showSuccessModal(status){
+  success_text.innerHTML= status;
+  success_modal.style.display = "block";
+}
+
+function showFailedModal(status){
+  failed_text.innerHTML= status;
+  failed_modal.style.display = "block";
+}
+
   
 function write_item_id() {
   write_item_id_modal.style.display = "none";
-    showModal('true');
+  showPlaceTagModal('true');
     var itemId = document.getElementById("item-id-input").value;
     ws.send('{"toDo": "write", "value": "'+itemId+'"}');
   }
@@ -76,12 +101,12 @@ function write_item_id() {
   }
 
   function do_check_in(value) {
-    showModal('true');
+    showPlaceTagModal('true');
     ws.send('{"toDo": "doCheckIn", "value": "'+value+'"}');
   }
   
   function show_item(){
-    showModal('true');
+    showPlaceTagModal('true');
   }
 
   function sendPing() {
@@ -112,11 +137,15 @@ function write_item_id() {
     }else{
       console.log(event.data);
       var json = JSON.parse(event.data);
-      showModal('false');
+      showPlaceTagModal('false');
       if(json.Done.localeCompare("read_item_id")==0){
         showItemId(json.value);
       }else{
-        alert(json.value);
+        if(json.value.includes('Success')){
+          showSuccessModal(json.value);
+        }else{
+          showFailedModal(json.value);
+        }
       }
     }
   }
@@ -125,3 +154,37 @@ function write_item_id() {
     document.getElementById("ws-status").innerHTML = "DISCONNECTED";
   }
   window.onload=onLoad;
+
+  function createCircleChart(percent, color, size, stroke) {
+    let svg = `<svg class="mkc_circle-chart" viewbox="0 0 36 36" width="${size}" height="${size}" xmlns="http://www.w3.org/2000/svg">
+        <path class="mkc_circle-bg" stroke="#eeeeee" stroke-width="${stroke * 0.5}" fill="none" d="M18 2.0845
+              a 15.9155 15.9155 0 0 1 0 31.831
+              a 15.9155 15.9155 0 0 1 0 -31.831"/>
+        <path class="mkc_circle" stroke="${color}" stroke-width="${stroke}" stroke-dasharray="${percent},100" stroke-linecap="round" fill="none"
+            d="M18 2.0845
+              a 15.9155 15.9155 0 0 1 0 31.831
+              a 15.9155 15.9155 0 0 1 0 -31.831" />
+        <text class="mkc_info" x="50%" y="50%" alignment-baseline="central" text-anchor="middle" font-size="8">${percent}%</text>
+    </svg>`;
+    return svg;
+}
+
+let charts_success = document.getElementsByClassName('success-div');
+let charts_failed = document.getElementsByClassName('failed-div');
+
+for(let i=0;i<charts_success.length;i++) {
+    let chart = charts_success[i];
+    let percent = chart.dataset.percent;
+    let color = ('color' in chart.dataset) ? chart.dataset.color : "#2F4F4F";
+    let size = ('size' in chart.dataset) ? chart.dataset.size : "100";
+    let stroke = ('stroke' in chart.dataset) ? chart.dataset.stroke : "1";
+    charts_success[i].innerHTML = createCircleChart(percent, color, size, stroke);
+}
+for(let i=0;i<charts_failed.length;i++) {
+  let chart = charts_failed[i];
+  let percent = chart.dataset.percent;
+  let color = ('color' in chart.dataset) ? chart.dataset.color : "#2F4F4F";
+  let size = ('size' in chart.dataset) ? chart.dataset.size : "100";
+  let stroke = ('stroke' in chart.dataset) ? chart.dataset.stroke : "1";
+  charts_failed[i].innerHTML = createCircleChart(percent, color, size, stroke);
+}
