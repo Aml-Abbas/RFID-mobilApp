410af8f39cd2fc27ac6c12fcfb5653f19cb667c1
Aml-Abbas <aml.abbas@hotmail.com>
2021-04-15 14:09:00 +0200

    commented serverThread

--- app/src/main/java/com/example/rfid_mobilapp/MainActivity.java
+++ app/src/main/java/com/example/rfid_mobilapp/MainActivity.java
@@ -54,6 +54,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         Log.d(TAG, "on Stop");
+        Intent intent = new Intent(this, NfcActivity.class);
+        startActivity(intent);
     }
 
     @Override
