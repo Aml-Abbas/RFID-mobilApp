56c51eba2ff023dfce68176873c27166614c9567
Aml-Abbas <aml.abbas@hotmail.com>
2021-04-20 15:22:20 +0200

    Filed not failed

--- Quria/javaScript.js
+++ Quria/javaScript.js
@@ -194,7 +194,7 @@ function write_item_id() {
       if(json.Done.localeCompare("read_item_id")==0){
         showItemId(json.value);
       }else{
-        if(json.value.includes('failed') || json.value.includes('lyckades inte')){
+        if(json.value.includes('Failed') || json.value.includes('lyckades inte')){
           showFailedModal(json.value);
         }else{
           showSuccessModal(json.value);
