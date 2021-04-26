e1ef1cc9375d11490d26d6baf6971ceca2beca41
Aml-Abbas <Aml-Abbas@users.noreply.github.com>
2021-04-26 15:27:58 +0000

    deploy: 1a305e052176839df7a519deac1320a43b7b9a45

--- javaScript.js
+++ javaScript.js
@@ -193,7 +193,7 @@ function write_item_id() {
       if(json.Done.localeCompare("read_item_id")==0){
         showItemId(json.value);
       }else{
-        if(json.value.includes('Failed') || json.value.includes('Misslyckades')){
+        if(json.value.includes('Failed') || json.value.includes('misslyckades')){
           showFailedModal(json.value);
         }else{
           showSuccessModal(json.value);
